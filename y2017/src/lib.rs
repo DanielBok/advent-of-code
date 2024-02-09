af207d9898312cb11c7a370c2eee711dd1b37921
Daniel Bok <daniel.bok@outlook.com>
2024-02-09 14:53:50 +0800

    2017/4

--- y2017/src/lib.rs
+++ y2017/src/lib.rs
@@ -1,12 +1,14 @@
 mod d1;
 mod d2;
 mod d3;
+mod d4;
 
 pub fn solve(day: usize) {
     match day {
         1 => { (d1::solve_a(), d1::solve_b()); }
         2 => { (d2::solve_a(), d2::solve_b()); }
         3 => { (d3::solve_a(), d3::solve_b()); }
+        4 => { (d4::solve_a(), d4::solve_b()); }
         0 => {
             for i in 1..=25 {
                 println!("Day {i}");
